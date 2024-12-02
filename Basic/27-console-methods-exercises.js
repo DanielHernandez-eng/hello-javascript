cff9a1a8312239c33a2009eb447bd7ef9ebc412d
Daniel Hernandez <daniel.hernandez@finvero.com>
2024-12-02 11:36:18 -0600

    final excersices

--- Basic/27-console-methods-exercises.js
+++ Basic/27-console-methods-exercises.js
@@ -4,21 +4,67 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=21421
 */
 
 // 1. Crea un función que utilice error correctamente
-
+function checarEdad(edad) {
+  edad < 18 ? console.error('No puedes acceder a esta página') : console.log('Bienvenido a la página')
+}
 // 2. Crea una función que utilice warn correctamente
-
+function sumar(a = 0, b = 0) {
+  if (a == 0 || b == 0) {
+    console.warn('No se puede sumar 0')
+  } else {
+    console.log(a + b);
+  }
+}
 // 3. Crea una función que utilice info correctamente
-
+function saludar(nombre) {
+  console.info(`Hola ${nombre}, bienvenido a la página`)
+}
 // 4. Utiliza table
+let errorDetails = [
+  { name: 'error info', code: 'X243324' }
+]
 
+console.table(errorDetails)
 // 5. Utiliza group
+console.group('Grupo datos')
+console.log('Datos PC');
+console.log('Motherboard: ASUS ROG STRIX a620-m');
+console.log('Procesador: AMD ryzen 3 2200');
+console.log('RAM: Corsair Vengance pro 2 X 16 GB');
+console.groupEnd()
 
 // 6. Utiliza time
+console.time("Tiempo de ejecucion");
+let suma = 0;
+for (let i = 0; i < 1000; i++) {
+  console.log(suma += i);
+}
+console.timeEnd("Tiempo de ejecucion")
 
 // 7. Valida con assert si un número es positivo
-
+const numero = 4;
+console.assert(numero > 0, "El numero no es positivo")
 // 8. Utiliza count
-
+console.count('funcion ejecutada')
+console.count('funcion ejecutada')
+console.count('funcion ejecutada')
+console.count('funcion ejecutada')
 // 9. Utiliza trace
+function funcA() {
+  funcB()
+}
 
+function funcB() {
+  console.trace("Seguimiento de la ejecución.")
+}
+function funcC() {
+  funcA()
+}
+function funcD() {
+  funcA()
+}
+funcC()
+funcD()
 // 10. Utiliza clear
+
+console.clear();
--- Basic/31-modules-exercises.js
+++ Basic/31-modules-exercises.js
@@ -4,21 +4,42 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=22720
 */
 
 // 1. Exporta una función
+export function prueba() {
+  console.log('funcion de prueba');
 
+}
 // 2. Exporta una constante
-
+export const a = 234238
 // 3. Exporta una clase
-
+export class prueba2 {
+  constructor() {
+    console.log('clase de prueba');
+  }
+}
 // 4. Importa una función
-
+import { prueba } from "./31-modules-exercises.js";
 // 5. Importa una constante
-
+import { a } from "./31-modules-exercises.js";
 // 6. Importa una clase
-
+import { prueba2 } from "./31-modules-exercises.js";
 // 7. Exporta una función, una constante y una clase por defecto (en caso de que lo permita)
+export function prueba5() {
+  console.log('tercera funcion');
+
+}
+
+export const numeroID = 23434234
+
+export default class prueba6 {
+  constructor() {
+    console.log('clase por defecto');
+  }
+}
 
 // 8. Importa una función, una constante y una clase por defecto (en caso de que lo permita)
-
+import prueba5, { numeroID, prueba6 } from "./31-modules-exercises.js";
 // 9. Exporta una función, una constante y una clase desde una carpeta
+import prueba5, { numeroID, prueba6 } from "../../practice-dev/Js/export-import-js/index.js";
 
 // 10. Importa una función, una constante y una clase desde un directorio diferente al anterior
+import prueba10, { identificador, registros } from "../../practice-dev/Js/Nodejs/hello-nodejs/src/index.js";
